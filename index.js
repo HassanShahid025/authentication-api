abeefd18d8da4f1e6232c703b1efac50d5cce253
hassan shahid <hassanshahid234@gmail.com>
2024-06-03 22:33:25 +0500

    changes

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./database/connect.js";
-import userRouter from "./routes/user-route.js";
 import authRouter from "./routes/auth-route.js";
 import cookieParser from "cookie-parser";
-import listingRouter from "./routes/listing-route.js";
 import cors from "cors";
 
 dotenv.config();
@@ -22,9 +20,9 @@ app.use(
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api/user", userRouter);
+
 app.use("/api/auth", authRouter);
-app.use("/api/listing", listingRouter);
+
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
@@ -46,4 +44,5 @@ const start = async () => {
     console.log(error);
   }
 };
+
 start();
